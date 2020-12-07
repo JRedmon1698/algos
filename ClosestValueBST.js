890d7fa035efd109785aba0a991ffb877a32d616
Joe Redmon <joesredmon@gmail.com>
2020-12-06 17:42:15 -0800

    adding bst closest val

---
+++ ClosestValueBST.js
@@ -0,0 +1,15 @@
+// Write a function that takes in a binary search tree and a target integer 
+// value and returns the closest value to that target valaue contained in the BST.
+
+function findClosestValueInBst(tree, target) {
+    // Write your code here.
+  }
+  
+  // This is the class of the input tree. Do not edit.
+  class BST {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
