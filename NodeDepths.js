5f42748a5e094c3b7ecd133f473a97eb07eeafa1
Joe Redmon <joesredmon@gmail.com>
2020-12-13 08:42:18 -0800

    setting up nodeDepth

---
+++ NodeDepths.js
@@ -0,0 +1,20 @@
+// The distance between a node in a Binary Tree and the tree's root is called the node's depth.
+
+// Write a function that takes in a Binary Tree and returns the sum of its nodes' depths.
+
+function nodeDepths(root) {
+    let depthSum = 0;
+
+    
+
+    return depthSum;
+  }
+  
+  // This is the class of the input binary tree.
+  class BinaryTree {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
