9ab5221eb9edb1f285b6ad9c5f7d8a6a2fdb626e
Joe Redmon <joesredmon@gmail.com>
2020-12-13 08:19:17 -0800

    closest val bst

--- ClosestValueBST.js
+++ ClosestValueBST.js
@@ -2,8 +2,24 @@
 // value and returns the closest value to that target valaue contained in the BST.
 
 function findClosestValueInBst(tree, target) {
-    // Write your code here.
-  }
+
+  const traverse = (tree, target, closest) => {
+		if (tree === null) {
+			return closest;
+		}
+		if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+			closest = tree.value;
+		}
+		if (target < tree.value) {
+			return traverse(tree.left, target, closest);
+		} else if (target > tree.value) {
+			return traverse(tree.right, target, closest);
+		} else {
+			return closest;
+		}
+	}
+	return traverse(tree, target, tree.value);
+}
   
   // This is the class of the input tree. Do not edit.
   class BST {
