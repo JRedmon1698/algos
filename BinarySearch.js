7e2e2e8721e9eedce81dffd6344c28de7e19e2c5
Joe Redmon <joesredmon@gmail.com>
2020-12-23 09:25:47 -0800

    binary search

---
+++ BinarySearch.js
@@ -0,0 +1,16 @@
+const binarySearch = (array, target) => {
+  let max = array[array.length - 1];
+  let min = array[0];
+  let mid = Math.floor((min + max) / 2);
+
+  while (mid !== target && min < max) {
+    if (target < mid) {
+      max = mid -1;
+    } else if (target > mid) {
+      min = mid + 1;
+    }
+    mid = Math.floor((min + max) / 2);
+    console.log(min, mid, max);
+  }
+  return mid === target ? array.indexOf(mid) : -1;
+};
