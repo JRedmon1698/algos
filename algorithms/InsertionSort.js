67bc71ced3222af9b154cecb1753cd08ae66743b
Joe Redmon <joesredmon@gmail.com>
2020-12-28 11:27:24 -0800

    insertion sort

--- algorithms/InsertionSort.js
+++ algorithms/InsertionSort.js
@@ -1,3 +1,12 @@
 function insertionSort(array) {
-  // Write your code here.
+  for (let i = 1; i < array.length; i++) {
+    let key = array[i];
+    let start = i - 1;
+    while (start >= 0 && array[start] > key) {
+      array[start + 1] = array[start];
+      start = start - 1;
+    }
+    array[start + 1] = key;
+  }
+  return array;
 }
