a3e83a37fb1a42ae6e5c0b9294c0c5242852420a
JRedmon1698 <joesredmon@gmail.com>
2021-09-21 10:16:21 -0700

    first dup val

---
+++ algorithms js/Intermediate/FirstDuplicateValue.js
@@ -0,0 +1,16 @@
+// Given an array of integers between 1 and navigator, inclusive, where n
+// is the legnth of the array, write a function that returns the first
+// integer that appears more than once (when the array is read from left 
+// to right).
+
+// In other words, out of all the integers that might occur more than once in the
+// input array, your function should return the one whose first duplicate value
+// has the minimum index.
+
+// If no integer appears more than oncuechange, your function should return -1.
+
+// Note that you are allowed to mutate the input array.
+
+const firstDuplicateValue = (array) => {
+
+};
