12b7feef451752a258c63cafe288a77f9ba78d0f
JRedmon1698 <joesredmon@gmail.com>
2021-09-29 14:58:24 -0700

    almost done bst

--- algorithms js/Intermediate/BinarySearchTreeClass.js
+++ algorithms js/Intermediate/BinarySearchTreeClass.js
@@ -6,12 +6,41 @@ class BST {
     }
   
     insert(value) {
+        if (value < this.value) {
+           if (!this.left) {
+               this.left = new BST(value);
+           } else {
+               this.left.insert(value);
+           }
+        }
+
+        if (value >= this.value) {
+            if (!this.right) {
+                this.right = new BST(value);;
+            } else {
+                this.right.insert(value);
+            }
+        }
 
       return this;
     }
   
     contains(value) {
+        if (this.value === value) return true;
 
+        if (this.value < value) {
+            if (this.right) {
+                return contains(this.right);
+            }
+        }
+
+        if (this.value > value) {
+            if (this.left) {
+                return this.contains(this.left);
+            }
+        }
+
+        return false;
     }
   
     remove(value) {
@@ -19,3 +48,12 @@ class BST {
       return this;
     }
   }
+
+  const tree = new BST(5);
+  console.log(tree);
+  console.log(tree.contains(5));
+  console.log(tree.insert(3));
+  console.log(tree.insert(6));
+  console.log(tree.insert(5));
+//   console.log(tree.remove());
+//   console.log(tree);
